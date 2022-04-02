44a68778fadfed7dc31fe2d17fbef955b2dc09f5
Juan Jose Restrepo <restrepotorojuanjose@gmail.com>
2022-04-01 22:18:42 -0500

    NEW:Create class daily Employee

--- TrabajoConstantesReferencias/DailyEmployee.cpp
+++ TrabajoConstantesReferencias/DailyEmployee.cpp
@@ -1,5 +1,39 @@
-//
-// Created by jjres on 29/03/2022.
-//
-
 #include "DailyEmployee.h"
+
+
+DailyEmployee::DailyEmployee(const string &first, const string &last, const string &ssn, int workedDays, double dailyWage)
+                : Employee(first, last, ssn){
+    setDailyWage(dailyWage);
+    setWorkedDays(workedDays);
+}
+
+int DailyEmployee::getWorkedDays() const{
+    return workedDays;
+}
+
+void DailyEmployee::setWorkedDays(const int &workedDays){
+    this -> workedDays = workedDays;
+}
+
+int DailyEmployee::getDailyWage() const{
+    return dailyWage;
+}
+
+void DailyEmployee::setDailyWage(const double &dailyWage){
+    this -> dailyWage = dailyWage;
+}
+
+double DailyEmployee::earnings() const{
+    if (getWorkedDays() > 0){
+        return getDailyWage() * getWorkedDays();
+    }else{
+        return 0;
+    }
+}
+
+void DailyEmployee::print() const{
+    cout << "daily employee: ";
+    Employee::print();
+    cout << "\n daily wage: " << getDailyWage() << "; days worked: " << getWorkedDays();
+}
+
--- TrabajoConstantesReferencias/DailyEmployee.h
+++ TrabajoConstantesReferencias/DailyEmployee.h
@@ -1,14 +1,36 @@
-//
-// Created by jjres on 29/03/2022.
-//
+#ifndef DAILYEMPLOYEE_H
+#define DAILYEMPLOYEE_H
 
-#ifndef TRABAJOREFERENCIAS_DAILYEMPLOYEE_H
-#define TRABAJOREFERENCIAS_DAILYEMPLOYEE_H
+#include <iostream>
+#include "Employee.h"
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
 
-class DailyEmployee {
+class DailyEmployee: public Employee{
+    private:
+        int workedDays;
+        double dailyWage;
+
+    public:
+        //Constructor
+        DailyEmployee(const string &, const string &, const string &, int, double = 0.0);
+
+        //Gets and Sets
+        int getWorkedDays() const;
+        void setWorkedDays(const int &);
+        int getDailyWage() const;
+        void setDailyWage(const double &);
+
+        //Virtual functions
+        double earnings() const;
+        void print() const;
+
 
 };
 
 
-#endif //TRABAJOREFERENCIAS_DAILYEMPLOYEE_H
+#endif
